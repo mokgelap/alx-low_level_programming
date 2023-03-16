fdd3881a9def432551ff93c6d91f5740f501240c
mokgelap <phineas.mokgola@vcontractor.co.za>
2023-03-16 13:45:08 -0700

    4-puts.c

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/**
+ * main - Printt a line with puts
+ * Return : 0  - when  the 0 is retuned the program is success
+ */
+int main(void)
+{
+puts("\"Programming is like building a multilingual puzzle");
+return (0);
+}
+
