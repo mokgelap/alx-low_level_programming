c6b345e234029d5fe81c694ffe6b4da8e84ba4c4
mokgelap <phineas.mokgola@vcontractor.co.za>
2023-03-22 16:18:22 -0700

    0-putchar.c

--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -1,6 +1,5 @@
-#ifndef main.h
-#define main.h
-
+#ifndef main_h
+#define main_h
 int _putchar(char);
 void print_alphabet(void);
 void print_alphabet_x10(void);
@@ -11,8 +10,8 @@ int _abs(int);
 int print_last_digit(int);
 void jack_bauer(void);
 void times_table(void);
-int add(int,int);
+int add(int, int);
 void print_to_98(int n);
 void print_times_table(int n);
-
 #endif
+
