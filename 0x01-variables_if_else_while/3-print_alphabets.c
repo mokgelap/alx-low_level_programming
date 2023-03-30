a15dab8bd41a698ffdc0fa0f72c3440a3410d666
mokgelap <phineas.mokgola@vcontractor.co.za>
2023-03-30 03:33:01 -0700

    3-print_alphabets.c

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -18,29 +18,3 @@ char alp[52] = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
 	return (0);
 }
 
-4-print_alphabt.c
-
-#include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
-
-/**
- * main - Prints the alphabet without q and e.
- *
- * Return: Always 0 (Success)
- */
-int main(void)
-{
-	int i;
-
-	for (i = 97; i < 123; i++)
-	{
-		if (i != 101 && i != 113)
-		{
-			putchar(i);
-		}
-	}
-	putchar('\n');
-	return (0);
-}
-
